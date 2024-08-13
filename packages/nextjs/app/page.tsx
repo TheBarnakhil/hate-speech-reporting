b71339666a678d089ac73221b4e4a971e8cc2d3b
TheBarnakhil <nsakhil763@gmail.com>
2024-08-13 13:06:36 +0530

    Added styles and made home page more responsive

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -11,23 +11,23 @@ const Home: NextPage = () => {
     <div className="drawer drawer-end">
       <input id="chat-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <div className="flex items-center flex-col pt-10 w-100vw h-100vh">
+        <div className="flex items-center flex-col pt-10 w-100vw h-100vh gap-y-[2rem]">
           <div className="mx-auto">
             <Image src={logo} alt="logo" width={120} height={120} />
           </div>
-          <div className="m-auto flex flex-row justify-around w-5/6 h-2/3">
+          <div className="m-auto flex flex-row gap-x-[4rem] w-5/6 h-2/3 justify-center">
             <div className="flex flex-col justify-between">
-              <Link href={"/about"} className="border-2 rounded-lg p-7 text-center">
+              <Link href={"/about"} className="border-2 text-indigo-700 dark:text-neutral-200 dark:border-slate-300 border-sky-400 rounded-lg p-7 text-center">
                 About Us
               </Link>
-              <label htmlFor="chat-drawer" className="drawer-button border-2 rounded-lg p-7 text-center">How can we help you?</label>
+              <label htmlFor="chat-drawer" className="drawer-button border-2 dark:text-neutral-200 text-indigo-700 dark:border-slate-300 border-sky-400 rounded-lg p-7 text-center">How can we help you?</label>
             </div>
             <Image src={homeImage} alt="logo" width={500} height={800} />
             <div className="flex flex-col justify-between">
-              <Link href={"/report"} className="border-2 rounded-lg p-7 text-center">
+              <Link href={"/report"} className="border-2 dark:text-neutral-200 text-indigo-700 dark:border-slate-300 border-sky-400 rounded-lg p-7 text-center">
                 Report your experience
               </Link>
-              <Link href={"/contact"} className="border-2 rounded-lg p-7 text-center">
+              <Link href={"/contact"} className="border-2 dark:text-neutral-200 text-indigo-700 dark:border-slate-300 border-sky-400 rounded-lg p-7 text-center">
                 Contact
               </Link>
             </div>
